1846ffba5d52345c2ca733e0a50b2b84fae36a0e
ptaushanov <pepi15032000@gmail.com>
2022-03-19 10:33:37 +0200

    feat: dark theme

--- src/themes/theme.js
+++ src/themes/theme.js
@@ -1,11 +1,35 @@
-import { DefaultTheme } from 'react-native-paper';
+import {
+    DefaultTheme as NavigationDefaultTheme,
+    DarkTheme as NavigationDarkTheme
+} from 'react-native-paper';
+
+import {
+    DefaultTheme as PaperDefaultTheme,
+    DarkTheme as PaperDarkTheme
+} from '@react-navigation/native';
+
+
 import { palette } from "./palette"
 
-export const defaultTheme = {
-    ...DefaultTheme,
+export const DefaultTheme = {
+    ...NavigationDefaultTheme,
+    ...PaperDefaultTheme,
     roundness: 2,
     colors: {
-        ...DefaultTheme.colors,
+        ...NavigationDefaultTheme.colors,
+        ...PaperDefaultTheme.colors,
+        primary: palette.primary,
+        accent: palette.accent
+    },
+};
+
+export const DarkTheme = {
+    ...NavigationDarkTheme,
+    ...PaperDarkTheme,
+    roundness: 2,
+    colors: {
+        ...NavigationDarkTheme.colors,
+        ...PaperDarkTheme.colors,
         primary: palette.primary,
         accent: palette.accent
     },
