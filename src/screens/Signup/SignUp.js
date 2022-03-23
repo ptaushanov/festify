cd6f625ddc1caea9b433c7f996ac4b5098fc8073
ptaushanov <pepi15032000@gmail.com>
2022-03-23 20:38:13 +0200

    featL firmik and yup validation

--- src/screens/Signup/SignUp.js
+++ src/screens/Signup/SignUp.js
@@ -4,6 +4,10 @@ import { useTheme, Surface, Button } from 'react-native-paper'
 import globalStyles from '../../styles/global'
 import StyledTextInput from '../../shared/TextInput/StyledTextInput'
 import { useNavigation } from '@react-navigation/native'
+import i18n from 'i18n-js'
+
+import { Formik } from 'formik'
+import * as Yup from "yup"
 
 const SignUp = () => {
     const { colors } = useTheme();
@@ -29,52 +33,101 @@ const SignUp = () => {
 
                 <Surface style={styles.surface}>
                     <Text style={styles.title}>Sign Up</Text>
-                    <View style={styles.inputContainer}>
-                        <StyledTextInput
-                            placeholder="Username"
-                            // value={null}
-                            // onChangeText={}
-                            style={styles.input}
-                        />
-                        <StyledTextInput
-                            placeholder="Email"
-                            // value={null}
-                            // onChangeText={}
-                            style={styles.input}
-                        />
-                        <StyledTextInput
-                            placeholder="Password"
-                            // value={null}
-                            // onChangeText={}
-                            style={styles.input}
-                        />
-                        <StyledTextInput
-                            placeholder="Confirm Password"
-                            // value={null}
-                            // onChangeText={}
-                            style={styles.input}
-                        />
-                    </View>
-                    <View style={styles.buttonContainer}>
-                        <Button
-                            mode="text"
-                            onPress={handleLoginPressed}
-                            style={styles.button}
-                            contentStyle={styles.buttonContent}
-                            color={colors.accent}
-                        >
-                            Login
-                        </Button>
-                        <Button
-                            mode="contained"
-                            // onPress={handleLogin}
-                            style={[styles.button, styles.buttonRight]}
-                            contentStyle={styles.buttonContent}
-                            color={colors.accent}
-                        >
-                            Sign Up
-                        </Button>
-                    </View>
+                    <Formik
+                        initialValues={{
+                            username: '',
+                            email: '',
+                            password: '',
+                            confirmPassword: ''
+                        }}
+
+                        validationSchema={Yup.object({
+                            username: Yup.string()
+                                .trim(i18n.t("auth:username-trim"))
+                                .strict(true)
+                                .min(4, i18n.t("auth:username-min").replace("{0}", 4))
+                                .max(512, i18n.t("auth:username-max").replace("{0}", 512))
+                                .required(i18n.t("auth:username-required")),
+
+                            email: Yup.string()
+                                .trim(i18n.t("auth:email-trim"))
+                                .strict(true)
+                                .email(i18n.t("auth:email-email"))
+                                .required(i18n.t("auth:email-required")),
+
+                            password: Yup.string()
+                                .trim(i18n.t("auth:password-trim"))
+                                .strict(true)
+                                .min(6, i18n.t("auth:password-min").replace("{0}", 6))
+                                .max(256, i18n.t("auth:password-max").replace("{0}", 256))
+                                .required(i18n.t("auth:password-required")),
+
+                            confirmPassword: Yup.string()
+                                .oneOf([Yup.ref('password'), null], i18n.t("auth:password-confirm"))
+                        })}
+                    >{
+                            ({ values, touched, errors, handleBlur, handleChange, handleSubmit }) => (
+                                <View>
+                                    <View style={styles.inputContainer}>
+                                        <StyledTextInput
+                                            placeholder="Username"
+                                            value={values.username}
+                                            onChangeText={handleChange("username")}
+                                            onBlur={handleBlur("username")}
+                                            style={styles.input}
+                                            error={touched.username && errors.username}
+                                            helperText={touched.username && errors.username}
+                                        />
+                                        <StyledTextInput
+                                            placeholder="Email"
+                                            value={values.email}
+                                            onChangeText={handleChange("email")}
+                                            onBlur={handleBlur("email")}
+                                            style={styles.input}
+                                            error={touched.email && errors.email}
+                                            helperText={errors.email && errors.email}
+                                        />
+                                        <StyledTextInput
+                                            placeholder="Password"
+                                            value={values.password}
+                                            onChangeText={handleChange("password")}
+                                            onBlur={handleBlur("password")}
+                                            style={styles.input}
+                                            error={touched.password && errors.password}
+                                            helperText={errors.password && errors.password}
+                                        />
+                                        <StyledTextInput
+                                            placeholder="Confirm Password"
+                                            value={values.confirmPassword}
+                                            onChangeText={handleChange("confirmPassword")}
+                                            onBlur={handleBlur("confirmPassword")}
+                                            style={styles.input}
+                                            error={touched.confirmPassword && errors.confirmPassword}
+                                            helperText={errors.confirmPassword && errors.confirmPassword}
+                                        />
+                                    </View>
+                                    <View style={styles.buttonContainer}>
+                                        <Button
+                                            mode="text"
+                                            onPress={handleLoginPressed}
+                                            style={styles.button}
+                                            contentStyle={styles.buttonContent}
+                                            color={colors.accent}
+                                        >
+                                            Login
+                                        </Button>
+                                        <Button
+                                            mode="contained"
+                                            onPress={handleSubmit}
+                                            style={[styles.button, styles.buttonRight]}
+                                            contentStyle={styles.buttonContent}
+                                            color={colors.accent}
+                                        >
+                                            Sign Up
+                                        </Button>
+                                    </View>
+                                </View>)}
+                    </Formik>
 
                 </Surface>
 
--- src/shared/TextInput/StyledTextInput.js
+++ src/shared/TextInput/StyledTextInput.js
@@ -1,15 +1,29 @@
-import { StyleSheet, TextInput } from 'react-native'
+import { StyleSheet, TextInput, View } from 'react-native'
 import React from 'react'
+import { Text, useTheme } from 'react-native-paper'
 
-const StyledTextInput = (props) => {
+const StyledTextInput = ({ error = false, helperText = '', helperTextStyles = {}, ...otherProps }) => {
+    const { colors } = useTheme();
     return (
-        <TextInput
-            {...props}
-            style={[
-                styles.input,
-                props.style,
-            ]}
-        />
+        <View>
+            <TextInput
+                {...otherProps}
+                style={[
+                    styles.input,
+                    otherProps.style,
+                ]}
+            />
+            <Text style={[
+                styles.helperText,
+                {
+                    display: helperText ? "flex" : "none",
+                    color: error ? colors.danger : "#333"
+                },
+                helperTextStyles
+            ]}>
+                {helperText}
+            </Text>
+        </View>
     )
 }
 
@@ -22,5 +36,8 @@ const styles = StyleSheet.create({
         paddingVertical: 10,
         borderRadius: 10,
         marginTop: 5
+    },
+    helperText: {
+        fontSize: 14
     }
 })
