742add67450d7ef02cd1767b3d62aca0baedb78d
ptaushanov <pepi15032000@gmail.com>
2023-05-15 13:50:40 +0300

    feat: seasons screen for web

--- src/screens/Lessons/components/SeasonCard.js
+++ src/screens/Lessons/components/SeasonCard.js
@@ -1,4 +1,5 @@
-import { StyleSheet, ImageBackground } from 'react-native'
+import { ImageBackground } from 'react-native'
+import StyleSheet from "react-native-media-query"
 import { Card, Surface, TouchableRipple, Text } from 'react-native-paper'
 import { MaterialIcons } from "@expo/vector-icons"
 import React from 'react'
@@ -16,13 +17,13 @@ const SeasonCard = ({ title = "", subtitle = "", image, onCardPress, locked = tr
                     onPress={locked ? null : onCardPress}
                     style={styles.ripple}
                     rippleColor={
-                        locked ? "transparent" : "rgba(200, 255, 255, .3)"
+                        locked ? "transparent" : "rgba(200, 255, 255, .4)"
                     }
                 >
                     <Card.Content style={[
                         styles.cardContent,
                         { backgroundColor: locked ? 'rgba(0,0,0, 0.65)' : 'rgba(0,0,0, 0.35)' }
-                    ]}>
+                    ]} dataSet={{ media: ids.cardContent }}>
                         <Text variant="titleLarge" style={styles.text}>
                             {title}
                         </Text>
@@ -43,21 +44,28 @@ const SeasonCard = ({ title = "", subtitle = "", image, onCardPress, locked = tr
 
 export default SeasonCard
 
-const styles = StyleSheet.create({
+const { styles, ids } = StyleSheet.create({
     surface: {
+        flex: 1,
         elevation: 4,
-        borderRadius: 10,
+        borderRadius: 10
     },
     card: {
+        flex: 1,
         width: "auto",
         height: "auto",
     },
     ripple: {
+        flex: 1,
         borderRadius: 10,
     },
     cardContent: {
+        flex: 1,
         paddingVertical: 20,
         borderRadius: 10,
+        "@media only screen and (min-width: 640px)": {
+            justifyContent: "flex-end"
+        }
     },
     cardImage: {
         borderRadius: 10,
