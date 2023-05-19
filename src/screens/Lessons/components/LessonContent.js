4bc74e3daff6d8dfb0de3de25a2aa0b28276311f
ptaushanov <pepi15032000@gmail.com>
2023-05-19 12:25:37 +0300

    style: styles for web

--- src/screens/Lessons/components/LessonContent.js
+++ src/screens/Lessons/components/LessonContent.js
@@ -1,5 +1,6 @@
-import { StyleSheet, View, Image, ScrollView, Dimensions } from 'react-native'
 import React from 'react'
+import StyleSheet from "react-native-media-query"
+import { View, Image, ScrollView, Dimensions, Platform } from 'react-native'
 import { Text } from 'react-native-paper'
 
 import LessonTitle from './LessonTitle'
@@ -15,15 +16,17 @@ const LessonContent = ({ title, content }) => {
                 )
             case "image":
                 const dimensions = Dimensions.get("window")
+                const margin_correction = 0.3 * dimensions.width
+                const height = Platform.OS === "web" ?
+                    Math.round((dimensions.width - margin_correction) * 9 / 16) :
+                    Math.round(dimensions.width * 9 / 16)
+
                 return (
                     <Image
                         key={index}
                         source={{ uri: item.value }}
                         resizeMode="contain"
-                        style={[
-                            styles.image,
-                            { height: Math.round(dimensions.width * 9 / 16) }
-                        ]}
+                        style={[styles.image, { height }]}
                     />
                 )
         }
@@ -34,7 +37,7 @@ const LessonContent = ({ title, content }) => {
             showsVerticalScrollIndicator={false}
             showsHorizontalScrollIndicator={false}
         >
-            <View style={styles.contentContainer}>
+            <View style={styles.contentContainer} dataSet={{ media: ids.contentContainer }}>
                 {title && <LessonTitle title={title} />}
                 {content && content.map((item, index) => (
                     constructComponent(item, index)
@@ -46,11 +49,14 @@ const LessonContent = ({ title, content }) => {
 
 export default LessonContent
 
-const styles = StyleSheet.create({
+const { styles, ids } = StyleSheet.create({
     contentContainer: {
         paddingHorizontal: 30,
         paddingTop: 10,
-        flex: 1
+        flex: 1,
+        "@media only screen and (min-width: 640px)": {
+            paddingHorizontal: "20%"
+        }
     },
     image: {
         borderRadius: 4,
