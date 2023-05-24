c4d02a7a1597dea31fa8f61560c412d9abbe0c79
ptaushanov <pepi15032000@gmail.com>
2023-05-24 15:33:55 +0300

    chore: save current state of project

--- src/screens/Lessons/components/LessonContent.js
+++ src/screens/Lessons/components/LessonContent.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import StyleSheet from "react-native-media-query"
-import { View, Image, ScrollView, Dimensions, Platform } from 'react-native'
+import { View, Image, ScrollView } from 'react-native'
 import { Text } from 'react-native-paper'
 
 import LessonTitle from './LessonTitle'
@@ -15,19 +15,14 @@ const LessonContent = ({ title, content }) => {
                     </Text>
                 )
             case "image":
-                const dimensions = Dimensions.get("window")
-                const margin_correction = 0.3 * dimensions.width
-                const height = Platform.OS === "web" ?
-                    Math.round((dimensions.width - margin_correction) * 9 / 16) :
-                    Math.round(dimensions.width * 9 / 16)
-
                 return (
-                    <Image
-                        key={index}
-                        source={{ uri: item.value }}
-                        resizeMode="contain"
-                        style={[styles.image, { height }]}
-                    />
+                    <View key={index} style={styles.imageContainer}>
+                        <Image
+                            source={{ uri: item.value }}
+                            resizeMode="contain"
+                            style={styles.image}
+                        />
+                    </View>
                 )
         }
     }
@@ -37,11 +32,14 @@ const LessonContent = ({ title, content }) => {
             showsVerticalScrollIndicator={false}
             showsHorizontalScrollIndicator={false}
         >
-            <View style={styles.contentContainer} dataSet={{ media: ids.contentContainer }}>
+            <View style={styles.lessonContainer} dataSet={{ media: ids.lessonContainer }}>
                 {title && <LessonTitle title={title} />}
-                {content && content.map((item, index) => (
-                    constructComponent(item, index)
-                ))}
+
+                <View style={styles.contentContainer} dataSet={{ media: ids.contentContainer }}>
+                    {content && content.map((item, index) => (
+                        constructComponent(item, index)
+                    ))}
+                </View>
             </View>
         </ScrollView>
     )
@@ -50,19 +48,38 @@ const LessonContent = ({ title, content }) => {
 export default LessonContent
 
 const { styles, ids } = StyleSheet.create({
-    contentContainer: {
+    lessonContainer: {
         paddingHorizontal: 30,
         paddingTop: 10,
         flex: 1,
         "@media only screen and (min-width: 640px)": {
-            paddingHorizontal: "20%"
+            paddingHorizontal: "10%",
         }
     },
-    image: {
-        borderRadius: 4,
+    contentContainer: {
+        flex: 1,
+        flexDirection: 'column',
+        "@media only screen and (min-width: 640px)": {
+            flexDirection: 'row',
+            flexWrap: 'wrap',
+            justifyContent: 'flex-start',
+            gap: 20
+        }
+    },
+    imageContainer: {
+        flex: 1,
+        width: "100%",
+        maxHeight: "50vh",
         marginVertical: 10,
     },
+    image: {
+        flex: 1,
+        borderRadius: 4,
+        width: '100%',
+        height: '100%',
+    },
     text: {
+        flex: 1,
         fontSize: 16,
         lineHeight: 20,
         paddingVertical: 10,
