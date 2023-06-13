a932940aa7c13822d853b851b8abb210d72e7def
ptaushanov <pepi15032000@gmail.com>
2023-06-13 11:52:09 +0300

    feat: notification token on sign up

--- src/services/authenticate.js
+++ src/services/authenticate.js
@@ -1,12 +1,15 @@
 import { auth, firestore } from "../../firebase.v9";
+import { registerForPushNotificationsAsync } from "../services/notifications";
 
-export const signUpUser = (newUser) => {
+export const signUpUser = async (newUser) => {
     const { username, email, password } = newUser;
     const unlocked_seasons = ["spring"]
     const unlocked_lessons = { spring: [0], summer: [0], autumn: [0], winter: [0] }
     const completed_lessons = { spring: [], summer: [], autumn: [], winter: [] }
     const current_lesson = { season: "spring", index: 0 }
     const collected_rewards = []
+
+    const notification_token = await registerForPushNotificationsAsync()
 
     const userData = {
         username,
@@ -16,17 +19,15 @@ export const signUpUser = (newUser) => {
         current_lesson,
         collected_rewards,
         last_reward_claim: 0,
-        xp: 0
+        xp: 0,
+        notification_token
     }
 
-    return auth.createUserWithEmailAndPassword(email, password)
-        .then(userCredentials => {
-            const { uid } = userCredentials.user
-            return firestore
-                .collection("users")
-                .doc(uid)
-                .set(userData)
-        })
+    const userCredentials = await auth.createUserWithEmailAndPassword(email, password)
+    const { uid } = userCredentials.user
+    return firestore.collection("users")
+        .doc(uid)
+        .set(userData)
 }
 
 export const logInUser = (newUser) => {
---
+++ src/services/notifications.js
@@ -0,0 +1,34 @@
+import { Platform } from "react-native";
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
+
+export async function registerForPushNotificationsAsync() {
+    let token;
+
+    if (Platform.OS === 'android') {
+        await Notifications.setNotificationChannelAsync('default', {
+            name: 'default',
+            importance: Notifications.AndroidImportance.MAX,
+            vibrationPattern: [0, 250, 250, 250],
+        });
+    }
+
+    if (Device.isDevice) {
+        const { status: existingStatus } = await Notifications.getPermissionsAsync();
+        let finalStatus = existingStatus;
+        if (existingStatus !== 'granted') {
+            const { status } = await Notifications.requestPermissionsAsync();
+            finalStatus = status;
+        }
+        if (finalStatus !== 'granted') {
+            alert('Failed to get push token for push notification!');
+            return;
+        }
+        token = (await Notifications.getExpoPushTokenAsync()).data;
+        console.log(token);
+    } else {
+        alert('Must use physical device for Push Notifications');
+    }
+
+    return token;
+}
